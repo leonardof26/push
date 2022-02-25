3ed475d4e61b15acf2462be27857d51808fe6133
Leonardo Fontes <leonardo.prado@g.globo>
2022-02-25 11:09:36 -0300

    changed service worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -31,9 +31,8 @@ export function register(config) {
     console.log('bbbbbbbbb')
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      const teste = `${process.env.PUBLIC_URL}/coisa.js`;
-      console.log('teste', teste)
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/coisa.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
@@ -53,7 +52,7 @@ export function register(config) {
       } else {
         // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);
-        registerValidSW(teste, config);
+        // registerValidSW(teste, config);
       }
     });
   }
